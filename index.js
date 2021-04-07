a87137ab3010e3a393fd5e6786b95b91f97a9e67
thinksaboutuser <thinksaboutuser@gmail.com>
2021-04-07 22:28:14 +0300

    removed firebase

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const expressApp = express()
 
 
 
-var firebase = require('firebase')
+// var firebase = require('firebase')
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.API_KEY || '',
-  authDomain: "mp-random.firebaseapp.com",
-  projectId: "mp-random",
-  storageBucket: "mp-random.appspot.com",
-  messagingSenderId: "748550940684",
-  appId: "1:748550940684:web:f32c6caa536e47cb5fa629",
-  measurementId: "G-CL9B439HMT",
-  databaseURL: "https://mp-random-default-rtdb.europe-west1.firebasedatabase.app/",
-  storageBucket: "bucket.appspot.com"
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY || '',
+//   authDomain: "mp-random.firebaseapp.com",
+//   projectId: "mp-random",
+//   storageBucket: "mp-random.appspot.com",
+//   messagingSenderId: "748550940684",
+//   appId: "1:748550940684:web:f32c6caa536e47cb5fa629",
+//   measurementId: "G-CL9B439HMT",
+//   databaseURL: "https://mp-random-default-rtdb.europe-west1.firebasedatabase.app/",
+//   storageBucket: "bucket.appspot.com"
+// };
 
 
 
-firebase.initializeApp(firebaseConfig)
+// firebase.initializeApp(firebaseConfig)
 
-let database = firebase.database()
+// let database = firebase.database()
 
 
 
@@ -85,19 +85,19 @@ bot.command("random", (ctx) => {
 
 
 
-  database.ref("users/"+ctx.from.username).set({
-    username: ctx.from.username,
-    first_name: ctx.from.first_name,
-    last_name: ctx.from.last_name
-  }, function(error) {
-    if (error) {
-      // The write failed...
-      console.log("Failed with error: " + error)
-    } else {
-      // The write was successful...
-      console.log("success")
-    }
-  })
+  // database.ref("users/"+ctx.from.username).set({
+  //   username: ctx.from.username,
+  //   first_name: ctx.from.first_name,
+  //   last_name: ctx.from.last_name
+  // }, function(error) {
+  //   if (error) {
+  //     // The write failed...
+  //     console.log("Failed with error: " + error)
+  //   } else {
+  //     // The write was successful...
+  //     console.log("success")
+  //   }
+  // })
 
 
   return ctx.reply("https://t.me/memasikpidorasik/" + getRandomInt(8000) );
